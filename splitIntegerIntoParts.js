14076873701b577f4482b2005f603899e44c6957
wesley <robinson.wesleyd@gmail.com>
2018-02-12 10:47:57 -0800

    Feb2018 - for interview assessment

---
+++ splitIntegerIntoParts.js
@@ -0,0 +1,24 @@
+// write a function that accepts `num`, a valid non-negative integer, and `parts`,
+// a positive integer representing how many parts to evenly split `num`,
+// and returns an array of these values
+
+// example usage:
+//      splitIntoInt(8, 4)  // [2,2,2,2]
+//      splitIntoInt(10, 4) // [2,2,3,3]
+//      splitIntoInt(20, 6) // [3,3,3,3,4,4]
+function splitIntoInt(num, parts) {
+    let array = Array(parts).fill(Math.floor(num / parts))
+
+    // get remainder and add it to the back of the array
+    let remainder = num % parts
+    while (remainder > 0) {
+        array[array.length - remainder] += 1
+        remainder -= 1
+    }
+
+    return array
+}
+
+console.log(splitIntoInt(8, 4))
+console.log(splitIntoInt(10, 4))
+console.log(splitIntoInt(20, 6))
